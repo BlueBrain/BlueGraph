0a8ca3fca218b77931506c102cc7d8a1b8949977
Eugenia Oshurko <yarutoua@gmail.com>
2020-09-30 16:42:33 +0200

    New nested path/summary graph/subgraph by type utils (#5)
    * Added an util for loading graphs
    
    * Bugfix in path printing
    
    * Added lost node import
    
    * Bugfix in load network
    
    * Division by zero bugfix
    
    * Removed deprecated 'weight' from paths
    
    * Added error on unknown strategy
    
    * Optimized path search for n=1
    
    * Fix in load_network
    
    * Added nested paths/path summary and subgraph by type
    
    * Clean outputs
    
    Co-authored-by: Oshurko Ievgeniia <oshurko@r1i7n1.bbp.epfl.ch>

--- kganalytics/utils.py
+++ kganalytics/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import networkx as nx
 
 
 def top_n(data_dict, n, smallest=False):
@@ -8,3 +9,29 @@ def top_n(data_dict, n, smallest=False):
     else:
         df = df.nlargest(n, columns=["value"])
     return(list(df["id"]))
+
+
+def subgraph_by_types(graph, type_data, types_to_include, types_to_exclude=None, include_nodes=None):
+    """Construct a subgraph of the source graph containing only the nodes of particular type."""
+    if include_nodes is None:
+        include_nodes = []
+    
+    if types_to_exclude is None:
+        types_to_exclude = []
+
+    def has_needed_type(n):
+        n_types = type_data[n]
+        include_found = False
+        exclude_found = False
+        for t in n_types:
+            if t in types_to_include:
+                include_found = True
+            if t in types_to_exclude:
+                exclude_found = True
+        return include_found and not exclude_found
+    
+    nodes = [
+        n for n in graph.nodes()
+        if has_needed_type(n) or n in include_nodes
+    ]
+    return nx.Graph(graph.subgraph(nodes))
