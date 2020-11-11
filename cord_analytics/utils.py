e0577bee548053274049cd68ed1c5e31010b1f21
Mohameth François SY <mohamethfrancois.sy@gmail.com>
2020-11-11 11:15:03 +0100

    Lower cased entity column of the linking dataframe (#23)
    * Lower cased entity column of the linking dataframe before merging it with extracted entities
    
    * Update utils.py

--- cord_analytics/utils.py
+++ cord_analytics/utils.py
@@ -277,6 +277,7 @@ def merge_with_ontology_linking(occurence_data,
     if linking is not None:
         linking = linking.rename(columns={"mention": "entity"})
         linking["concept"] = linking["concept"].apply(lambda x: x.lower())
+        linking["entity"] = linking["entity"].apply(lambda x: x.lower()) # The provided occcurence_data is expected to be lower cased and the merge is performed on the 'entity' column and not the column one.
         
         print("Merging the occurrence data with the ontology linking...")
     
