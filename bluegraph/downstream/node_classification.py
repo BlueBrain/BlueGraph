ce725edef42e16a507d43d118800936fe1664d28
Eugenia Oshurko <yarutoua@gmail.com>
2021-02-16 17:13:15 +0100

    Added node similarity/classifier interfaces, added notebook

--- bluegraph/downstream/node_classification.py
+++ bluegraph/downstream/node_classification.py
@@ -1,13 +1,48 @@
 class NodeClassifier(object):
+    """A minimal wrapper around a classification model.
 
-    def __init__(self, model):
+    This wrapper alows to build classification models of PGFrame nodes.
+    """
+
+    def __init__(self, model, feature_vector_prop=None, feature_props=None):
         self.model = model
+        self.feature_vector_prop = feature_vector_prop
+        self.feature_props = feature_props
 
-    def fit():
-        pass
+    def _concatenate_feature_props(self, pgframe, nodes):
+        if self.feature_props is None or len(self.feature_props) == 0:
+            raise ValueError
+        return pgframe.nodes(
+            raw_frame=True).loc[nodes, self.feature_props].to_numpy()
 
-    def transform():
-        pass
+    def fit(self, pgframe, train_nodes=None, labels=None, label_prop=None):
+        # If no train nodes provided, use all nodes of the input graph
+        if train_nodes is None:
+            train_nodes = pgframe.nodes()
 
-    def fit_transform():
-        pass
+        # If no labels provided, try to use a label property
+        if labels is None:
+            if label_prop not in pgframe.node_properties():
+                raise ValueError()
+            labels = pgframe.get_node_property_values(
+                label_prop).loc[train_nodes].tolist()
+
+        # If no feature vector property provided,
+        # try to concatenate feature_props
+        if self.feature_vector_prop:
+            data = pgframe.get_node_property_values(
+                self.feature_vector_prop).loc[train_nodes].tolist()
+        else:
+            data = self._concatenate_feature_props(pgframe, train_nodes)
+        self.model.fit(data, labels)
+
+    def predict(self, pgframe, predict_nodes=None):
+        # If no prediction nodes provided, use all nodes of the input graph
+        if predict_nodes is None:
+            predict_nodes = pgframe.nodes()
+        if self.feature_vector_prop:
+            data = pgframe.get_node_property_values(
+                self.feature_vector_prop).loc[predict_nodes].tolist()
+        else:
+            data = self._concatenate_feature_props(pgframe, predict_nodes)
+        return self.model.predict(data)
--- bluegraph/downstream/similarity.py
+++ bluegraph/downstream/similarity.py
@@ -30,34 +30,14 @@ class SimilarityProcessor(object):
             self._add(initial_vectors, initial_index)
 
     def _preprocess_vectors(self, vectors):
-        if not isinstance(vectors, np.ndarray):
+        if isinstance(vectors, pd.Series):
+            vectors = np.array(vectors.to_list())
+        elif not isinstance(vectors, np.ndarray):
             vectors = np.array(vectors)
         vectors = vectors.astype(np.float32)
         if self.similarity == "cosine":
             faiss.normalize_L2(vectors)
         return vectors
-
-    def _query_existing(self, existing_indices, k=10):
-        if self.index is not None:
-            existing_indices = self.index.get_indexer(existing_indices)
-        x = [self._model.reconstruct(int(i)) for i in existing_indices]
-        return self._query_new(x, k)
-
-    def _query_new(self, vectors, k=10):
-        vectors = self._preprocess_vectors(vectors)
-        distance, int_index = self._model.search(vectors, k)
-        return distance, int_index
-
-    def _add(self, vectors, vector_indices=None):
-        vectors = self._preprocess_vectors(vectors)
-        if vector_indices is not None:
-            for i in vector_indices:
-                if i in self.index:
-                    raise SimilarityProcessor.IndexException(
-                        "Index '{}' already exists".format(i))
-            self.index = self.index.append(pd.Index(vector_indices))
-
-        self._model.add(vectors)
 
     def _initialize_model(self, initial_vectors=None):
 
@@ -83,6 +63,28 @@ class SimilarityProcessor(object):
             self._model.make_direct_map()
         else:
             self._model = index
+
+    def _query_existing(self, existing_indices, k=10):
+        if self.index is not None:
+            existing_indices = self.index.get_indexer(existing_indices)
+        x = [self._model.reconstruct(int(i)) for i in existing_indices]
+        return self._query_new(x, k)
+
+    def _query_new(self, vectors, k=10):
+        vectors = self._preprocess_vectors(vectors)
+        distance, int_index = self._model.search(vectors, k)
+        return distance, int_index
+
+    def _add(self, vectors, vector_indices=None):
+        vectors = self._preprocess_vectors(vectors)
+        if vector_indices is not None:
+            for i in vector_indices:
+                if i in self.index:
+                    raise SimilarityProcessor.IndexException(
+                        "Index '{}' already exists".format(i))
+            self.index = self.index.append(pd.Index(vector_indices))
+
+        self._model.add(vectors)
 
     def get_similar_points(self, vectors=None, vector_indices=None,
                            existing_indices=None, k=10,
@@ -120,3 +122,41 @@ class SimilarityProcessor(object):
 
     class QueryException(Exception):
         pass
+
+
+class NodeSimilarityProcessor(object):
+
+    def __init__(self, pgframe, vector_property, similarity="euclidean"):
+        self.graph = pgframe
+        self.vector_property = vector_property
+        self.similarity = similarity
+        initial_vectors = self.graph.get_node_property_values(
+            vector_property).tolist()
+        self.dimension = len(initial_vectors[0])
+
+        self.processor = SimilarityProcessor(
+            dimension=self.dimension,
+            similarity=self.similarity,
+            initial_vectors=initial_vectors,
+            initial_index=self.graph.nodes())
+
+    def get_similar_nodes(self, input_nodes, k=10):
+        neighbors, dist = self.processor.get_similar_points(
+            existing_indices=input_nodes, k=k)
+        result = {}
+        for i, ns in enumerate(neighbors):
+            result[input_nodes[i]] = dict(zip(ns, dist[i]))
+        return result
+
+    def update_similarity_model(self):
+        new_nodes = [
+            node
+            for node in self.graph.nodes()
+            if node not in self.processor.index
+        ]
+        print(new_nodes)
+        if len(new_nodes) > 0:
+            self.processor._add(
+                vectors=self.graph.get_node_property_values(
+                    self.vector_property).loc[new_nodes].tolist(),
+                vector_indices=new_nodes)
