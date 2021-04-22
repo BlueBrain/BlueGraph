34db96a5bd29a8c07e8fa48365fbc7e54d829f7c
Eugenia Oshurko <yarutoua@gmail.com>
2021-04-22 14:46:58 +0200

    Added v0.1.1 Release notes (#57)
    Co-authored-by: Mohameth François SY <mohamethfrancois.sy@gmail.com>

--- bluegraph/downstream/link_prediction.py
+++ bluegraph/downstream/link_prediction.py
@@ -13,6 +13,10 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+"""This module is inspired by the following StellarGraph demo
+(licensed under Apache 2)
+https://stellargraph.readthedocs.io/en/stable/demos/link-prediction/node2vec-link-prediction.html.
+"""
 from .data_structures import ElementClassifier
 
 import math
