1f79e3920b23e8eae6a9da13e7efdab893b92e8d
Eugenia Oshurko <yarutoua@gmail.com>
2021-10-26 10:08:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
             "pyjwt==1.7.1",
             "ipywidgets",
             "neo4j",
-            "stellargraph>=1.2.0"
+         #   "stellargraph>=1.2.0"
         ],
         "docs": [
             "sphinx", "sphinx-bluebrain-theme",
