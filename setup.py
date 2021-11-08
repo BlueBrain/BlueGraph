6294ab724376e1be02b8d752fcb94b06209eaf11
Eugenia Oshurko <yarutoua@gmail.com>
2021-11-08 09:35:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
         "numpy>=1.20.1",
         "pandas>=1.3.0",
-        "sklearn",
+        "scikit-learn>=0.24.2",
         "scipy",
         "matplotlib",
         "nltk",
