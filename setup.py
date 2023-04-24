65642141bceed4fffb569c0326e393428ddac9d7
Mouffok Sarah <mouffok@bbpv2.epfl.ch>
2023-04-24 12:44:50 +0200

    upper bound on pandas version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ],
     install_requires=[
         "numpy>=1.20.1",
-        "pandas>=1.3.0",
+        "pandas>=1.3.0,<2.0",
         "scikit-learn>=1.0.2",
         "scipy",
         "matplotlib",
